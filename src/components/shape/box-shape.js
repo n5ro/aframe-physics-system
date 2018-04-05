da2908db46e998b15b74c530a006452531a73c6f
Kevin Lee <kevin@infinite-lee.com>
2018-04-05 16:44:33 -0700

    remove *-shape helper components

--- src/components/shape/box-shape.js
+++
@@ -1,5 +0,0 @@
-var Shape = require('./shape');
-
-var BoxShape = AFRAME.utils.extend({}, Shape.definition);
-
-module.exports = AFRAME.registerComponent('box-shape', BoxShape);
--- src/components/shape/sphere-shape.js
+++
@@ -1,9 +0,0 @@
-var Shape = require('./shape');
-
-var SphereShape = AFRAME.utils.extend({}, Shape.definition);
-
-SphereShape.schema = AFRAME.utils.extend({}, Shape.definition.schema, {
-  shape: {default: 'sphere', oneOf: ['box', 'sphere', 'cylinder']}
-});
-
-module.exports = AFRAME.registerComponent('sphere-shape', SphereShape);
